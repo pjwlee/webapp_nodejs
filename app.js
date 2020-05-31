76f37ffef87a8b16f6b7ca9afaa8ad2e5777544d
pjwlee <pjwlee@gmail.com>
2020-05-31 09:22:51 -0700

    create main

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.set('port', port);
 
 // the login page
 app.get('/', function(req, res) {
-  fs.access('./login.html', function(err){
+  fs.access('./main.html', function(err){
     if (err) {
       res.statusCode=404;
       res.end();
       return;
     }
     else {
-      fs.readFile('./login.html', 'utf-8', function (err, data) {
+      fs.readFile('./main.html', 'utf-8', function (err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
         console.log("loaded: login");

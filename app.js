7d4332c6cbcad34395bcd72df71acbaf0a01ca72
pjwlee <pjwlee@gmail.com>
2020-06-04 16:24:28 -0700

    add GET for login

--- app.js
+++ app.js
@@ -12,7 +12,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', port);
 
 
-// the login page
+// function test(loc) {
+//   location = './' + loc + '.html';
+//   pat = '/' + loc;
+//   app.get(pat, function(req, res) {
+//     fs.access(location, function(err){
+//       if (err) {
+//         res.statusCode=404;
+//         res.end();
+//         return;
+//       }
+//       else {
+//         fs.readFile(location, 'utf-8', function (err, data) {
+//           res.writeHead(200, {'Content-Type': 'text/html'});
+//           res.end(data);
+//           console.log("loaded: login");
+//         });
+//       }
+//     });
+//   });
+// }
+
+// app.get('/login', function(req, res) {
+//   test('login');
+// });
+
+
+// main page
 app.get('/', function(req, res) {
   fs.access('./main.html', function(err){
     if (err) {
@@ -30,6 +56,24 @@ app.get('/', function(req, res) {
   });
 });
 
+
+// login page
+app.get('/login', function(req, res) {
+	fs.access('./login.html', function(err){
+		if (err) {
+			res.statusCode=404;
+			res.end();
+			return;
+		}
+		else {
+			fs.readFile('./login.html', 'utf-8', function (err, dat) {
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.end(dat);
+				console.log("loaded: login")
+			});
+		}
+	});
+});
 
 // a server starts listening
 app.listen(app.get('port'), function() {
